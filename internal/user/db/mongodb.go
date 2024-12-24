f45ca2e7e7c2e14dc6dc7c516c2a43adcbccbd17
Vyacheslav <nodegopher@gmail.com>
2024-12-24 22:54:27 +0300

    k

--- internal/user/db/mongodb.go
+++ internal/user/db/mongodb.go
@@ -2,16 +2,34 @@ package db
 
 import (
 	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 
 	"github.com/morheus9/go_rest/internal/user"
 	"github.com/morheus9/go_rest/pkg/logging"
 )
 
 type db struct {
+	collection *mongo.Collection
+	logger     *logging.Logger
 }
 
-func (d *db) Create(context.Context, user.User) (string, error) {
-	panic("implement me")
+func (d *db) Create(ctx context.Context, user user.User) (string, error) {
+	d.logger.Debug("create user")
+	result, err := d.collection.InsertOne(ctx, user)
+	if err != nil {
+		return "", fmt.Errorf("failed to create user %v", err)
+	}
+	d.logger.Debug("convert insertedID to ObjectID")
+	oid, ok := result.InsertedID.(primitive.ObjectID)
+	if ok {
+		oid.Hex()
+		return oid.Hex(), nil
+	}
+	d.logger.Trace(user)
+	return "", fmt.Errorf("failed to convert objectID to hex")
 }
 
 func (d *db) FindOne(context.Context, string) (user.User, error) {
@@ -25,6 +43,10 @@ func (d *db) Update(context.Context, user.User) error {
 func (d *db) Delete(context.Context, string) error {
 	panic("implement me")
 }
-func NewStorage(collection string, logger *logging.Logger) user.Storage {
-	return &db{}
+func NewStorage(database *mongo.Database, collection string, logger *logging.Logger) user.Storage {
+
+	return &db{
+		collection: database.Collection(collection),
+		logger:     logger,
+	}
 }
--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,16 @@
 package user
+
+import (
+	"context"
+
+	"github.com/morheus9/go_rest/pkg/logging"
+)
+
+type Service struct {
+	storage Storage
+	logger  *logging.Logger
+}
+
+func (s *Service) Create(ctx context.Context, dto CreateUserDTO) {
+
+}
